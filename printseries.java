c6992312917b78cc719793eff5f0e7eb38f40192
sudhakaran1995 <sudhakarvijay41@gmail.com>
2020-11-30 17:01:15 +0530

    jenkins

---
+++ printseries.java
@@ -0,0 +1,17 @@
+ public class printseries {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 char a='i';
+	
+		for(char i='a';i<='z';++i){
+		
+		
+			System.out.println(" "+i);
+		
+			
+		}
+
+	}  
+
+}
